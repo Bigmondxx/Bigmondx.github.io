c3c11f7040857e64ff2a9cdb74ba6d1b1e6201c6
Bigmondxx <117779003+Bigmondxx@users.noreply.github.com>
2022-11-10 16:53:19 -0500

    Add files via upload

---
+++ project3/app.js
@@ -0,0 +1,64 @@
+var main = function () {
+
+var volume;
+
+//The PLAY button
+
+$('#play').click(function () {
+
+$('#message').text("Playing track");
+
+$('#player').trigger("play");
+
+});
+
+//The PAUSE button
+
+$('#pause').click(function () {
+
+$('#message').text("Track Paused");
+
+$('#player').trigger("pause");
+
+});
+
+//This will assign the variable muted with true or false
+
+muted = $("player").prop('muted');
+
+
+$('#mute').click(function () {
+
+$('#message').text("Track Muted");
+
+$("#player").prop('muted', true);
+
+});
+
+$('#unmute').click(function () {
+
+$('#message').text("Track Unmuted");
+
+$("#player").prop('muted', false);
+
+});
+
+$('#volumeup').click(function () {
+var volume = $("#player").prop("volume")+0.1;
+if(volume >1){
+volume = 1;
+}
+$("#player").prop("volume",volume);
+});
+
+$('#volumedown').click(function () {
+var volume = $("#player").prop("volume")-0.1;
+if(volume <0){
+volume = 0;
+}
+$("#player").prop("volume",volume);
+});
+}
+
+$(document).ready(main);
+
